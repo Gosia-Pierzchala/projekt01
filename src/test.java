954f4d2d730e11b27268f22c7cb83b7cc43e067d
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-04 09:43:11 +0200

    Dodanie tekstu "i commitowania"

--- src/test.java
+++ src/test.java
@@ -2,6 +2,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Uczę");
         System.out.println("się");
-        System.out.print("programowania");
+        System.out.print("programowania\n");
+        System.out.println("i");
+        System.out.print("commitowania");
     }
 }
