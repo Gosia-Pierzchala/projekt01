83ea0a5d10f46cca617e374e3a8f0ae7e02d7949
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-05 10:34:14 +0200

    Usunięcie pierwszego zdania.

--- src/test.java
+++ src/test.java
@@ -1,10 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Uczę");
-        System.out.println("się");
-        System.out.print("programowania\n");
-        System.out.println("i");
-        System.out.println("commitowania.");
         System.out.println("Odrabiam " + "zadanie " + "domowe.");
         System.out.println("Moje dane:");
         System.out.println("Imię i nazwisko: Małgorzata Pierzchała");
