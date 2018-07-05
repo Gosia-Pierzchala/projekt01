2776e944dba58dbb97c2915ffed72eb3cd87b07a
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-05 12:44:59 +0200

    Aktualizacja danych.

--- src/test.java
+++ src/test.java
@@ -1,8 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Odrabiam " + "zadanie " + "domowe.");
-        System.out.println("Moje dane:");
-        System.out.println("Imię i nazwisko: Małgorzata Pierzchała");
-        System.out.print("Nr tel. 784 968 307");
+        System.out.println("Moje dane");
+        System.out.println("Imię i nazwisko: ");
+        System.out.println("Małgorzata Pierzchała");
+        System.out.println("Nr tel. ");
+        System.out.print("111 222 333");
     }
 }
